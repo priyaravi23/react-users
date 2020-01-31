e5fd6d0cc368544fae19533aec53f77de5bfd25b
Priya Ravi <priyaravi23@gmail.com>
2020-01-31 15:20:46 -0800

    Users app with ability to add, delete, and update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,197 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './app/users/users.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        formState: {
+            id: '',
+            firstName: "",
+            lastName: "",
+            email: "",
+            mode: "submit"
+        },
+        users: [
+            {
+                id: 0,
+                firstName: "priya",
+                lastName: "ravi",
+                email: "pppravi@gmail.com",
+                updating: false
+            }
+        ]
+    };
+
+    resetFormState = () => {
+        this.setState({
+            formState: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                mode: "submit",
+                id: ""
+            }
+        });
+    };
+
+    onChange = event => {
+        this.setState({
+            formState: {
+                ...this.state.formState,
+                [event.target.name]: event.target.value
+            }
+        });
+    };
+
+    onSubmit = event => {
+        const { users, formState } = this.state;
+        event.preventDefault();
+        const firstName = event.target.querySelector("input[name='firstName']")
+            .value;
+        const lastName = event.target.querySelector("input[name='lastName']")
+            .value;
+        const email = event.target.querySelector("input[name='email']").value;
+        if (formState.mode === "submit") {
+            this.setState({
+                users: [
+                    ...this.state.users,
+                    {
+                        firstName,
+                        lastName,
+                        email,
+                        updating: false,
+                        id: this.state.users[this.state.users.length - 1].id + 1
+                    }
+                ]
+            });
+        } else if (formState.mode === "edit") {
+            const index = users.find((user)=> user.id === formState.id).id;
+            users[index] = {
+                firstName,
+                lastName,
+                email,
+                updating: false,
+                id: users[index].id
+            }
+            this.setState({
+                users: [...users]
+            });
+        }
+
+        this.resetFormState();
+    };
+
+    updateUser = key => {
+        let { users } = this.state;
+        users[key].updating = true;
+
+        this.setState({
+            formState: { ...this.state.users[key], mode: "edit" },
+            users
+        });
+    };
+
+    deleteUser = key => {
+        let { users } = this.state;
+        users.splice(key, 1);
+        this.setState({
+            users: [...users]
+        });
+    };
+
+    render() {
+        const { users, formState } = this.state;
+        return (
+            <div>
+                <Form
+                    formState={formState}
+                    onChange={this.onChange}
+                    onSubmit={this.onSubmit}
+                />
+                <Table
+                    users={users}
+                    updateUser={this.updateUser}
+                    deleteUser={this.deleteUser}
+                />
+            </div>
+        );
+    }
 }
+
+const Table = ({ users = [], updateUser, deleteUser }) => {
+    return (
+        <div className="table">
+            <div className="table-header">
+                <div className="row">
+                    <div className="column">First Name</div>
+                    <div className="column">Last Name</div>
+                    <div className="column">Email</div>
+                    <div className="column">Options</div>
+                </div>
+            </div>
+            <div className="table-body">
+                {users.map((user, key) => {
+                    return (
+                        <div className={`row ${user.updating ? "updating" : ""}`}>
+                            <div className="column">{user.firstName}</div>
+                            <div className="column">{user.lastName}</div>
+                            <div className="column">{user.email}</div>
+                            <div className="column">
+                                <button
+                                    className="icon"
+                                    onClick={() => updateUser(key)}
+                                >
+                                    <i class="fa fa-edit" />
+                                </button>
+                                <button className="icon">
+                                    <i
+                                        class="fas fa-user-minus"
+                                        onClick={() => deleteUser(key)}
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+const Field = ({ label = "", name = "", value = "", onChange }) => {
+    return (
+        <div className="field">
+            <label htmlFOR={name}>{label}</label>
+            <input type="text" name={name} value={value} onChange={onChange} />
+        </div>
+    );
+};
+
+const Form = ({ formState, onChange, onSubmit }) => {
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <fieldset>
+                <Field
+                    name="firstName"
+                    label="First Name"
+                    value={formState.firstName}
+                    onChange={onChange}
+                />
+                <Field
+                    name="lastName"
+                    label="Last Name"
+                    value={formState.lastName}
+                    onChange={onChange}
+                />
+                <Field
+                    name="email"
+                    label="Email"
+                    value={formState.email}
+                    onChange={onChange}
+                />
+            </fieldset>
+            <button>{formState.mode}</button>
+        </form>
+    );
+};
 
 export default App;
